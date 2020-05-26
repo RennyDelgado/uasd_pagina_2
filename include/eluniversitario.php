bb528416e75dda2b5eb31819b5824d9a2b0af37f
Renny Delgado <renny_delgado@hotmail.com>
2020-05-26 10:53:31 -0400

    #11 cambio de la seccion el universitario

---
+++ include/eluniversitario.php
@@ -0,0 +1,74 @@
+<div class="container universitario">
+
+  <div class="row">
+
+  		<div class="col-lg-12 mt-5 p-0">
+  			<h3 class="nuestros_egresados">EL UNIVERSITARIO</h3>
+  			<hr class="regla_titulo"></hr>
+  		</div>
+  	</div>
+
+<div class="row">
+
+<div class="col-lg-12 p-0">
+
+<div id="universitario" class="carousel slide" data-ride="carousel">
+
+  <div class="carousel-inner banner-universitario">
+
+    <div class="carousel-item active" style="background-image:url(img/fondo_noticia_universitario-1.jpg)">
+    <div class="carousel-caption d-md-block">
+    <h1 class="titulo">UASD y el Instituto Dominicano para la Calidad
+      ejecutarán proyectos de investigación y programas académicos</h1>
+
+         <a href="#" class="ampliar-universitario">AMPLIAR EN EL UNIVERSITARIO</a>
+       </div>
+
+  </div>
+
+    <div class="carousel-item" style="background-image:url(img/uno.jpg)">
+
+      <div class="carousel-caption d-md-block">
+        <h1 class="titulo">UASD y el Instituto Dominicano para la Calidad
+          ejecutarán proyectos de investigación y programas académicos</h1>
+
+             <a href="#" class="ampliar-universitario">AMPLIAR EN EL UNIVERSITARIO</a>
+           </div>
+       </div>
+
+    </div>
+
+    <div class="carousel-item" style="background-image:url(img/tres.jpg)">
+
+      <div class="carousel-caption d-md-block">
+
+        <h1 class="titulo">UASD y el Instituto Dominicano para la Calidad
+          ejecutarán proyectos de investigación y programas académicos</h1>
+
+             <a href="#" class="ampliar-universitario">AMPLIAR EN EL UNIVERSITARIO</a>
+           </div>
+       </div>
+
+    </div>
+
+
+  <a class="carousel-control-prev" href="#universitario" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+
+  <a class="carousel-control-next" href="#universitario" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+
+
+</div>
+
+  </div>
+</div>
+
+
+</div>
+
+</div>
