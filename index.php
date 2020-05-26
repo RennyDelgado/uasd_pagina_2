b6d88fa6942019084ef8c92bc965a182669e5599
Renny Delgado <renny_delgado@hotmail.com>
2020-05-26 09:10:39 -0400

    #5 creacion del archivo index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php   
+include('include/head.php');
+ ?>
+
+<?php   
+include('include/slider.php');
+ ?>
+
+<?php   
+include('include/servicios.php');
+ ?>
+
+<?php   
+include('include/rectoria.php');
+?>
+
+<?php   
+include('include/facultades.php');
+?>
+
+
+
+
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br>
+
+<?php   
+include('include/footer.php');
+ ?>
+
+
+
+
+
+
+
+
