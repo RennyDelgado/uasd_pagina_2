2b7181d75efc76b707abce11c7c396c009fb11c7
Renny Delgado <renny_delgado@hotmail.com>
2020-05-26 09:15:00 -0400

    #7 se agrego la agenda al codigo

---
+++ include/agenda.php
@@ -0,0 +1,133 @@
+<div class="container-fluid fondo_agenda">
+
+<div class="container agenda">
+
+    <div class="row">
+
+    <div class="col-12">
+
+			<div class="col-lg-5 col-md-5 col-xs-5 float-left agenda_dias" style="background-image:url(img/image_agenda.png)">
+        <div class="agenda_color">
+
+            <div class="dias">
+                <ul clas="list-group">
+                    <li class="activo"><a href="#">01</a></li>
+                    <li><a href="#">02</a></li>
+                    <li><a href="#">03</a></li>
+                    <li><a href="#">04</a></li>
+                    <li><a href="#">05</a></li>
+                    <li><a href="#">06</a></li>
+                    <li><a href="#">07</a></li>
+                </ul>
+					</div>
+
+          <div class="datos">
+              <h3 class="fecha">Junio de 2020</h3>
+              <p class="hora_detalle"> 10:00 a.m.</p>
+              <p class="dealle"> Entrega de Título Doctor Honoris Causa al
+                Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+              <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y
+                Sociales</p>
+           </div>
+          </div>
+			</div>
+
+			<div class="col-lg-7 col-md-7 col-xs-7 float-left detalle-dias">
+          <h3>agenda | JUEVES, 20 de junio de 2019</h3>
+          <hr class="regla">
+
+<div id="demo-agenda" class="carousel slide" data-ride="carousel">
+
+          <ol class="carousel-indicators">
+             <li data-target="#demo-agenda" data-slide-to="0" class="active"></li>
+             <li data-target="#demo-agenda" data-slide-to="1"></li>
+             <li data-target="#demo-agenda" data-slide-to="2"></li>
+           </ol>
+
+<div class="carousel-inner">
+
+<div class="carousel-item active">
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora juridica-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora ciencia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonett, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora economia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+</div>
+<div class="carousel-item">
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora juridica-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora ciencia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora economia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+</div>
+<div class="carousel-item">
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora juridica-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora ciencia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+          <div class="detalles-horas">
+          <p class="hora_agenda">10:00 A.M.</p> <p class="hora economia-hora"></p>
+          <p class="texto_agenda">Entrega de Título Doctor Honoris Causa al
+            Señor Ignacio Ramonet, de la facultad de Humanidades</p>
+          <img src="img/icono_agenda.svg" class="img-fluid pull-xs-left lugar_icono" alt="...">
+          <p class="ubicacion">Paraninfo Facultad de Ciencias Económicas y Sociales</p>
+          </div>
+</div>
+
+
+</div>
+
+
+</div>
+
+        </div>
+			</div>
+
+		</div>
+	</div>
+
+
+</div>
+</div>
