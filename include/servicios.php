b6aa1665cdd54d86a01d90dd1655f118113226c4
Renny Delgado <renny_delgado@hotmail.com>
2020-08-06 19:56:55 -0400

    Cambios de fuentes y estilos en los servicios de la pagina

--- include/servicios.php
+++ include/servicios.php
@@ -1,11 +1,12 @@
 <div class="container servicios">
-	
+
 <div class="row mt-5">
-		
+
 <div class="card col-lg-2 col-md-4 col-xs-4">
 	  <img class="img-fluid" src="img/autoservicio.svg">
 		  <div class="card-body">
     		<h5 class="card-title">AUTO SERVICIO</h5>
+				<p class="servicio"><a href="#">Estudiantes</a> <a href="#">Docentes</a> </p>
   	</div>
 </div>
 
@@ -13,6 +14,7 @@
 	  <img class="img-fluid" src="img/Admiciones.svg">
 		  <div class="card-body">
     		<h5 class="card-title">ADMISIONES</h5>
+				<p class="servicio"><a href="#">Nuevo Ingreso</a> <a href="#">Viejo Ingreso</a></p>
   	</div>
 </div>
 
@@ -20,6 +22,8 @@
 	  <img class="img-fluid" src="img/Sismologico.svg">
 		  <div class="card-body">
     		<h5 class="card-title">SISMOLOGICO UASD</h5>
+					<p><a href="#">Actividad Sísmica de R.D.</a></p>
+
   	</div>
 </div>
 
@@ -27,6 +31,7 @@
 	  <img class="img-fluid" src="img/uasd_virtual.svg">
 		  <div class="card-body">
     		<h5 class="card-title">UASD VIRTUAL</h5>
+				<p><a href="#">Campus Virtual de la UASD</a></p>
   	</div>
 </div>
 
@@ -34,6 +39,7 @@
 	  <img class="img-fluid" src="img/Calendario_Academico.svg">
 		  <div class="card-body">
     		<h5 class="card-title">CALENDARIO ACADEMICO</h5>
+				<p><a href="#">Inicio y término de semestre, inscripciones, investidura</a></p>
   	</div>
 </div>
 
@@ -41,6 +47,8 @@
 	  <img class="img-fluid" src="img/Nuestro_campus.svg">
 		  <div class="card-body">
     		<h5 class="card-title">NUESTRO CAMPUS</h5>
+				<p><a href="#">Conoce Nuestro Campus y lugares de docencia</a></p>
+
   	</div>
 </div>
 
@@ -55,4 +63,3 @@
 </center>
 
 </div>
- 
